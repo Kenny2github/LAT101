f71bedbc92490828d351e2459f8f908680480739
AbyxDev <ken@abyx.dev>
2023-10-26 16:41:47 -0400

    Full conjugation table test

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from typing import get_args
 from notes import NounVocab, Noun, AdjVocab, Adjective, VerbVocab, Verb, Tense, \
-    Case, Number
+    Case, Number, Voice, Mood
 
 def declension_table(noun: NounVocab) -> None:
     width = max(len(str(Noun(case, number, noun)))
@@ -30,39 +30,54 @@ def modification_table(adj: AdjVocab) -> None:
                 print(str(Adjective(adj, Noun(case, number, noun))).center(width), end='')
             print()
 
-def conjugation_table(verb: VerbVocab) -> None:
+def conjugation_table(
+    verb: VerbVocab, voice: Voice = 'active', mood: Mood = 'indicative'
+) -> None:
     tenses = list(Tense)
-    width = max(len(str(Verb(person, number, tense, 'active', 'indicative', verb)))
+    if mood == 'subjunctive':
+        del tenses[2], tenses[-1] # no future subjunctives
+    present_tenses = tenses[:len(tenses)//2]
+    perfect_tenses = tenses[len(tenses)//2:]
+    width = max(len(str(Verb(person, number, tense, voice, mood, verb)))
                 for person in (1, 2, 3) for number in get_args(Number) for tense in tenses)
     width = max(width, max(len(tense.value) for tense in tenses))
     width += 1 # add a space
-    print('singular'.center(width * 3) + 'plural'.center(width * 3))
-    for tense in (*tenses[:3], *tenses[:3]):
+    print('singular'.center(width * len(present_tenses))
+          + 'plural'.center(width * len(present_tenses)))
+    for tense in (*present_tenses, *present_tenses):
         print(tense.value.center(width), end='')
-    print('\n' + '-' * (width * 6))
+    print('\n' + '-' * (width * len(present_tenses) * 2))
     for person in (1, 2, 3):
         for number in get_args(Number):
-            for tense in tenses[:3]:
+            for tense in present_tenses:
                 print(str(Verb(person, number, tense,
-                               'active', 'indicative', verb)).center(width), end='')
+                               voice, mood, verb)).center(width), end='')
         print()
 
-    print('singular'.center(width * 3) + 'plural'.center(width * 3))
-    for tense in (*tenses[3:], *tenses[3:]):
+    print('singular'.center(width * len(perfect_tenses))
+          + 'plural'.center(width * len(perfect_tenses)))
+    for tense in (*perfect_tenses, *perfect_tenses):
         print(tense.value.center(width), end='')
-    print('\n' + '-' * (width * 6))
+    print('\n' + '-' * (width * len(perfect_tenses) * 2))
     for person in (1, 2, 3):
         for number in get_args(Number):
-            for tense in tenses[3:]:
+            for tense in perfect_tenses:
                 print(str(Verb(person, number, tense,
-                               'active', 'indicative', verb)).center(width), end='')
+                               voice, mood, verb)).center(width), end='')
         print()
+
+def full_conj_table(verb: VerbVocab) -> None:
+    for voice in get_args(Voice):
+        for mood in get_args(Mood):
+            if mood == 'imperative':
+                continue # not yet implemented
+            print(voice.upper(), mood.upper())
+            print('=' * (len(voice) + len(mood) + 1))
+            conjugation_table(verb, voice, mood)
 
 def test_verbs() -> None:
     from vocab import incipiō, sentiō
-    conjugation_table(incipiō)
-    print()
-    conjugation_table(sentiō)
+    full_conj_table(sentiō)
 
 def test_nouns() -> None:
     from vocab import servus
